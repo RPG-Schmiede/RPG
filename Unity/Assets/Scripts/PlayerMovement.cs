4d5edd47e05692cf509c235956d7611469660a4e
Stephan <stephan.schueritz@web.de>
2016-04-19 22:39:15 +0200

    Add Character Sprites, Animation & Movement, Camera-Follow
    Add: Sprites
    Add: Animation for Player
    Add: Player-Movement
    Add: Camera-Follow

---
+++ Unity/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    Rigidbody2D rBody;
+    Animator anim;
+
+    Vector2 movementVector;
+
+	// Use this for initialization
+	void Start ()
+    {
+        rBody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        movementVector = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        if(movementVector != Vector2.zero)
+        {
+            anim.SetBool("is_walking", true);
+            anim.SetFloat("input_x", movementVector.x);
+            anim.SetFloat("input_y", movementVector.y);
+        }
+        else
+        {
+            anim.SetBool("is_walking", false);
+        }
+
+        rBody.MovePosition(rBody.position + movementVector * Time.deltaTime);
+    }
+}
