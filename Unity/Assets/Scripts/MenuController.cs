7bf98597ebf7781345051e6a8fa155ae1c7526be
Stephan <stephan.schueritz@web.de>
2016-05-19 00:40:16 +0200

    Add: MainMenu, Add: Dummy Graphic

---
+++ Unity/Assets/Scripts/MenuController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuController : MonoBehaviour
+{
+	public void ShowContinueGame()
+    {
+        // Open Continue-Window
+    }
+
+    public void ShowNewGame()
+    {
+        // Open NewGame-Window
+    }
+
+    public void ShowSettings()
+    {
+        // Open Settings-Window
+    }
+
+    public void QuitGame()
+    {
+        // Quit App
+        Application.Quit();
+    }
+}
